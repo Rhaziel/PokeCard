aea0b481d0441574b75205752518cb024875a86a
Jeremy <jerec_11@yahoo.com.ar>
2020-08-07 19:08:10 -0300

    agregada la imagen del pokemon

--- js/app.js
+++ js/app.js
@@ -11,17 +11,25 @@ function cargarNombres(e) {
      const cantidad = document.getElementById('numero1').value;
      const inicial = document.getElementById('numero2').value;
      */
-     let url = '';
-     url += 'https://pokeapi.co/api/v2/';
-     
+
+
      if (tipoSeleccionado !== '') {
-          url += `type/${tipoSeleccionado}`
+          obtenerDatos(tipoSeleccionado);
+
      }
+
+}
+
+function obtenerDatos(tipoSeleccionado) {
+
+     let url = 'https://pokeapi.co/api/v2/type/' + tipoSeleccionado;
+
      const xhr = new XMLHttpRequest();
 
      xhr.open('GET', url, true)
 
      xhr.onload = function () {
+
           if (this.status === 200) {
                const contenido = JSON.parse(this.responseText);
                //obtiene un número random para el arreglo de pokemones
@@ -32,7 +40,47 @@ function cargarNombres(e) {
                htmlNombre += `<li>${contenido.pokemon[num].pokemon.name}</ul>`;
                //muestra el resultado de la generación de pokemon
                document.getElementById('resultado').innerHTML = htmlNombre;
+               obtenerImagen(contenido.pokemon[num].pokemon.name)
           }
      }
      xhr.send();
+
+
+
+}
+
+function obtenerImagen(nombre) {
+
+     let url = 'https://pokeapi.co/api/v2/pokemon/' + nombre;
+
+     const xhr = new XMLHttpRequest();
+
+     xhr.open('GET', url, true)
+
+     xhr.onload = function () {
+          if (this.status === 200) {
+
+               const contenido = JSON.parse(this.responseText);
+               //agrega el nombre del pokemon aleatorio a una etiqueta html
+
+               if (contenido.sprites.other['official-artwork'].front_default === null) {
+                    //Fixeo por si la imagen no se encuentra
+                    let mensaje = "Imagen no disponible para el pokemon " + nombre
+                    let htmlNombre = `<h3>${mensaje} </h3>`;
+                    //muestra el resultado de la generación de pokemon
+                    document.getElementById('resultado').innerHTML = htmlNombre;
+
+               } else {
+                    let htmlNombre = `<img src="${contenido.sprites.other['official-artwork'].front_default}"/>`;
+                    //muestra el resultado de la generación de pokemon
+                    document.getElementById('resultado').innerHTML = htmlNombre;
+
+               }
+
+          }
+
+     }
+     xhr.send();
+
+
 }
